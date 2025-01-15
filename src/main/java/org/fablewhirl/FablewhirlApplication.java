4f8a075d3f40330b046944e9dbde1aaae0e13831
Fl1s <maksima7th@gmail.com>
2025-01-15 23:32:15 +0700

    Second Initial

---
+++ src/main/java/org/fablewhirl/FablewhirlApplication.java
@@ -0,0 +1,13 @@
+package org.fablewhirl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FablewhirlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FablewhirlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fablewhirl/FablewhirlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fablewhirl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FablewhirlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
