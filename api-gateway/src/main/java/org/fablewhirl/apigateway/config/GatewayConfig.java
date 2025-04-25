a22db864b9e60e40a7571d4bd38754e530b1b51b
Fl1s <maksima7th@gmail.com>
2025-04-25 17:41:58 +0700

    refactor: dockerfiles & docker-compose & api-gateway

--- api-gateway/src/main/java/org/fablewhirl/apigateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/org/fablewhirl/apigateway/config/GatewayConfig.java
@@ -20,61 +20,64 @@ public class GatewayConfig {
     @Bean
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
         return builder.routes()
+
+                // Swagger
                 .route("auth_service_docs", r -> r.path("/aggregate/auth-service/v3/api-docs")
                         .filters(f -> f.rewritePath("/aggregate/auth-service/v3/api-docs", "/v3/api-docs"))
-                        .uri("http://auth-service:8081"))
+                        .uri("lb://auth-service"))
 
                 .route("user_service_docs", r -> r.path("/aggregate/user-service/v3/api-docs")
                         .filters(f -> f.rewritePath("/aggregate/user-service/v3/api-docs", "/v3/api-docs"))
-                        .uri("http://user-service:8082"))
+                        .uri("lb://user-service"))
 
                 .route("character_service_docs", r -> r.path("/aggregate/character-service/v3/api-docs")
                         .filters(f -> f.rewritePath("/aggregate/character-service/v3/api-docs", "/v3/api-docs"))
-                        .uri("http://character-service:8083"))
+                        .uri("lb://character-service"))
 
                 .route("thread_service_docs", r -> r.path("/aggregate/thread-service/v3/api-docs")
                         .filters(f -> f.rewritePath("/aggregate/thread-service/v3/api-docs", "/v3/api-docs"))
-                        .uri("http://thread-service:8084"))
+                        .uri("lb://thread-service"))
 
                 .route("comment_service_docs", r -> r.path("/aggregate/comment-service/v3/api-docs")
                         .filters(f -> f.rewritePath("/aggregate/comment-service/v3/api-docs", "/v3/api-docs"))
-                        .uri("http://comment-service:8085"))
+                        .uri("lb://comment-service"))
 
                 .route("notification_service_docs", r -> r.path("/aggregate/notification-service/v3/api-docs")
                         .filters(f -> f.rewritePath("/aggregate/notification-service/v3/api-docs", "/v3/api-docs"))
-                        .uri("http://notification-service:8086"))
+                        .uri("lb://notification-service"))
 
-                // Main Service Routes
+                // Основные маршруты
                 .route("auth_service", r -> r.path("/api/v1/auth/**")
                         .filters(f -> f.circuitBreaker(c -> c.setName("authServiceCircuitBreaker")
                                 .setFallbackUri("forward:/fallbackRoute")))
-                        .uri("http://auth-service:8081"))
+                        .uri("lb://auth-service"))
 
                 .route("user_service", r -> r.path("/api/v1/users/**")
                         .filters(f -> f.circuitBreaker(c -> c.setName("userServiceCircuitBreaker")
                                 .setFallbackUri("forward:/fallbackRoute")))
-                        .uri("http://user-service:8082"))
+                        .uri("lb://user-service"))
 
                 .route("character_service", r -> r.path("/api/v1/characters/**")
                         .filters(f -> f.circuitBreaker(c -> c.setName("characterServiceCircuitBreaker")
                                 .setFallbackUri("forward:/fallbackRoute")))
-                        .uri("http://character-service:8083"))
+                        .uri("lb://character-service"))
 
                 .route("thread_service", r -> r.path("/api/v1/threads/**")
                         .filters(f -> f.circuitBreaker(c -> c.setName("threadServiceCircuitBreaker")
                                 .setFallbackUri("forward:/fallbackRoute")))
-                        .uri("http://thread-service:8084"))
+                        .uri("lb://thread-service"))
 
                 .route("comment_service", r -> r.path("/api/v1/comments/**")
                         .filters(f -> f.circuitBreaker(c -> c.setName("commentServiceCircuitBreaker")
                                 .setFallbackUri("forward:/fallbackRoute")))
-                        .uri("http://comment-service:8085"))
+                        .uri("lb://comment-service"))
 
                 .route("notification_service", r -> r.path("/api/v1/notifications/**")
                         .filters(f -> f.circuitBreaker(c -> c.setName("notificationServiceCircuitBreaker")
                                 .setFallbackUri("forward:/fallbackRoute")))
-                        .uri("http://notification-service:8086"))
+                        .uri("lb://notification-service"))
 
+                // Фоллбэк
                 .route("fallbackRoute", r -> r.path("/fallbackRoute")
                         .filters(f -> f.rewritePath("/fallbackRoute", "/"))
                         .uri("forward:/fallback"))
