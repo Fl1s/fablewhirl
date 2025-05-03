07e260d14cef88baae3fa6bd62faccef9fa2dd63
Fl1s <maksima7th@gmail.com>
2025-05-03 19:50:49 +0700

    feat: init redis cache, add it in thread service

---
+++ thread-service/src/main/java/org/fablewhirl/thread/config/RedisConfig.java
@@ -0,0 +1,37 @@
+package org.fablewhirl.thread.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.fablewhirl.thread.dto.ThreadReadDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        Jackson2JsonRedisSerializer<ThreadReadDto> serializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper, objectMapper.getTypeFactory().constructType(ThreadReadDto.class));
+
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer));
+
+        return RedisCacheManager.builder(connectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+}
--- thread-service/src/main/java/org/fablewhirl/thread/dto/ThreadReadDto.java
+++ thread-service/src/main/java/org/fablewhirl/thread/dto/ThreadReadDto.java
@@ -5,7 +5,7 @@ import lombok.Data;
 import java.time.LocalDateTime;
 
 @Data
-public class ThreadDto {
+public class ThreadReadDto {
     private String threadId;
     private String userId;
     private String title;
